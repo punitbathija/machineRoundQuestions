95175acbf3d861f37fa593de967e47ae1ee3c150
Punit Bathija <punit644@gmail.com>
2023-05-02 14:20:13 +0530

    Added a new script with new examples to understand

---
+++ index.js
@@ -0,0 +1,13 @@
+// const btn = document.getElementById("btn");
+
+// console.log("start");
+// setTimeout(function () {
+//   console.log("Callback");
+// }, 5000);
+// console.log("end");
+
+// btn.addEventListener("click", function () {
+//   alert("You have been bamboozeled");
+// });
+
+let startDate;
