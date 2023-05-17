82d9cf7d595f0379ba19c767300485e278ad693c
Punit Bathija <punit644@gmail.com>
2023-05-17 16:29:38 +0530

    Added some new practise questions

--- nopeek.js
+++ nopeek.js
@@ -39,3 +39,30 @@ for (let i = 1; i <= rows; i++) {
 }
 
 console.log(output);
+
+// Print prime numbers from 1 to 100
+
+function printPrimes(n) {
+  const primeNumbers = [];
+  let count = 0;
+  let num = 2;
+  while (count < n) {
+    if (isPrime(num)) {
+      primeNumbers.push(num);
+      count++;
+    }
+    num++;
+  }
+  console.log(primeNumbers);
+  function isPrime(num) {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+printPrimes(100);
